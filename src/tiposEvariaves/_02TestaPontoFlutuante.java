78d4933366f8037f27a79e777c29e96f1b42f648
Paulojoserc <pjrc83@gmail.com>
2023-02-09 23:58:27 -0300

    Caracteres

--- src/tiposEvariaves/_02TestaPontoFlutuante.java
+++ src/tiposEvariaves/_02TestaPontoFlutuante.java
@@ -1,5 +1,11 @@
 package tiposEvariaves;
 
 public class _02TestaPontoFlutuante {
-
+public static void main(String[] args) {
+	double salario = 1250.70;
+	System.out.println("meu salario é " + salario);
+	int divisao = 5 / 2;
+	System.out.println(divisao);
+	
+}
 }
