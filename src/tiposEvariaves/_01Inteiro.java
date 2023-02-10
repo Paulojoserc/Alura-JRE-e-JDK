4192e8f7cecd90f1f3a197a955f4998c78d38721
Paulojoserc <pjrc83@gmail.com>
2023-02-09 22:07:17 -0300

    Initial commit

---
+++ src/tiposEvariaves/_01Inteiro.java
@@ -0,0 +1,20 @@
+package tiposEvariaves;
+
+public class _01Inteiro {
+	public static void main(String[] args) {
+        System.out.println("ola novo teste");
+
+        int idade;
+        idade = 37;
+
+        System.out.println("a idade é " + idade);        
+        
+        idade = 30 + 10;
+
+        System.out.println(idade);
+
+        idade = (7 * 5) +  2;
+
+        System.out.println(idade);
+    }
+}
