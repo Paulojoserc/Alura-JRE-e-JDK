b0a389fd5f31f87314bb98acdb6b94919be12337
Paulo Costa <pjrc83@gmail.com>
2023-02-14 17:47:36 -0300

    FEAT - While somador

---
+++ src/controle/WhileSoma.java
@@ -0,0 +1,18 @@
+package controle;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+public class WhileSoma {
+	public static void main(String[] args) {
+		int contador = 0;
+		int total = 0;
+		
+		while(contador <= 10) {
+			total+= contador;
+			contador++;
+		}
+		System.out.println(total);
+	}
+}
