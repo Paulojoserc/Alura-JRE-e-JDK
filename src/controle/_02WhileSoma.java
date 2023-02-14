c9c4612f2c88cf4bf596927da4c48ce6f4fb7ecb
Paulo Costa <pjrc83@gmail.com>
2023-02-14 18:01:26 -0300

    FEAT - FOR

--- src/controle/_02WhileSoma.java
+++ src/controle/_02WhileSoma.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
-public class WhileSoma {
+public class _02WhileSoma {
 	public static void main(String[] args) {
 		int contador = 0;
 		int total = 0;
