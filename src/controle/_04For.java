30002b4c6678bae613e9a9c9960d484ce916d220
Paulo Costa <pjrc83@gmail.com>
2023-02-15 17:59:14 -0300

    For Taboada

---
+++ src/controle/_04For.java
@@ -0,0 +1,16 @@
+package controle;
+
+public class _04For {
+	
+
+        public static void main(String[] args) {
+            for(int multiplicador = 1; multiplicador <= 10; multiplicador++) {
+                for(int contador = 1; contador <= 10; contador++) {
+                    System.out.println("Tabuada de "+ multiplicador+" x "+contador+" = "+ (multiplicador * contador));
+                    
+                }
+                System.out.println();
+            }
+        }
+
+}
